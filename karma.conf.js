911c938f70ec67cb9c5e14e8e7f2bcbd1225e1b5
Yuri Kroz <yuri@kilometer.io>
2016-07-07 11:40:26 +0300

    Walking through analytics.js...

--- karma.conf.js
+++ karma.conf.js
@@ -3,26 +3,72 @@
 
 module.exports = function(config) {
   config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+    // frameworks to use
+    frameworks: ['browserify', 'jasmine'],
+
+    // list of files / patterns to load in the browser
     files: [
-      { pattern: 'test/*', included: false },
-      'test/*.test.js'
+      // {pattern: 'bower_components/**/*.js', included: false},
+      // {pattern: 'lib/**/*.js', included: true},
+      // { pattern: 'test/**/*.test.js', included: true }
+      'test/**/*.test.js'
+      // {pattern: 'tests/**/*Spec.js', included: true}
     ],
 
-    browsers: ['PhantomJS'],
+    // list of files to exclude
+    exclude: [
+    ],
 
-    frameworks: ['browserify', 'mocha'],
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karka-ie-launcher`)
+    // browsers: ['Firefox', 'PhantomJS', 'chrome_without_security'],
+    browsers: ['chrome_without_security'],
 
-    reporters: ['spec'/* , 'coverage' */],
+    customLaunchers: {
+      chrome_without_security: {
+        base: 'Chrome',
+        flags: ['--disable-web-security']
+      }
+    },
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_INFO
+    logLevel: config.LOG_INFO,
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage', 'spec'
+    reporters: ['spec', 'coverage'],
 
     preprocessors: {
       'test/**/*.js': 'browserify'
     },
 
+    /*
     client: {
       mocha: {
         grep: process.env.GREP
       }
     },
+    */
 
     browserify: {
       debug: true
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var Analytics = require('@segment/analytics.js-core').constructor;
-var each = require('component-each');
+// var each = require('component-each');
 var integration = require('@segment/analytics.js-integration');
 var sandbox = require('@segment/clear-env');
 var tester = require('@segment/analytics.js-integration-tester');
@@ -11,10 +11,13 @@ describe('Kilometer.io', function() {
   var kilometerIntegration;
   var analytics;
   var options = {
-    app_id: 'abcdefghi1234567890'
+    app_id: 'abcdefghi1234567890',
+    endPoint: 'events.stage.kilometer.io',
+    webUrl: 'app.stage.kilometer.io/events/save/'
   };
 
   beforeEach(function() {
+    console.log('Entered test Kilometer.io > beforeEach()');
     analytics = new Analytics();
     kilometerIntegration = new KilometerIntegration(options);
     analytics.use(KilometerIntegration);
@@ -23,60 +26,118 @@ describe('Kilometer.io', function() {
   });
 
   afterEach(function() {
-    analytics.restore();
-    analytics.reset();
-    kilometerIntegration.reset();
+    console.log('Entered test Kilometer.io > afterEach()');
+    analytics = null;
+    kilometerIntegration = null;
+    // analytics.restore();
+    // analytics.reset();
+    // kilometerIntegration.reset();
     sandbox();
   });
 
   it('should have the right settings', function() {
+    console.log('Entered test Kilometer.io > it(should have the right settings)');
     analytics.compare(KilometerIntegration, integration('Kilometer.io')
-        .global('KilometerIntegration')
+        .global('kilometerIntegration')
         .option('app_id', '')
-        .tag('<script src="//static.kilometer.io/js/events-api-client2.js">'));
+        .tag('<script src="//s3.amazonaws.com/kilometer.public/events-api-client3.js">'));
   });
 
   describe('before loading', function() {
     beforeEach(function() {
-      analytics.stub(KilometerIntegration, 'load');
+      console.log('Entered test Kilometer.io > before loading > beforeEach()');
+      analytics.stub(kilometerIntegration, 'load');
+    });
+
+    afterEach(function() {
+      console.log('Entered test Kilometer.io > before loading > afterEach()');
+      if (typeof window.Kilometer !== 'undefined' || window.Kilometer !== null) {
+        window.Kilometer = null;
+      }
     });
 
     describe('#initialize', function() {
       it('should create window.Kilometer', function() {
+        console.log('Entered test Kilometer.io > before loading > #initialize > it(should create window.Kilometer)');
         analytics.assert(!window.Kilometer);
         analytics.initialize();
         analytics.assert(window.Kilometer);
       });
 
-
+      /*
       it('should stub window.kilometerIntegration with the right methods', function() {
+        console.log('Entered test Kilometer.io > before loading > #initialize > it(should stub window.kilometerIntegration with the right methods)');
         // transmitEvent identify addUser setUserProperties increaseUserProperty decreaseUserProperty
         var methods = ['track', 'initialize', 'identify', 'loaded'];
-        analytics.assert(!window.KilometerIntegration);
+        analytics.assert(!window.kilometerIntegration);
         analytics.initialize();
         each(methods, function(method) {
-          analytics.assert(window.KilometerIntegration[method]);
+          analytics.assert(window.kilometerIntegration[method]);
         });
       });
-
+      */
 
       it('should stub window.Kilometer with the right methods', function() {
+        console.log('Entered test Kilometer.io > before loading > #initialize > it(should stub window.Kilometer with the right methods)');
         // transmitEvent identify addUser setUserProperties increaseUserProperty decreaseUserProperty
         var methods = ['transmitEvent', 'identify', 'addUser', 'setUserProperties', 'increaseUserProperty', 'decreaseUserProperty'];
         analytics.assert(!window.Kilometer);
         analytics.initialize();
-        each(methods, function(method) {
+        analytics.assert(window.Kilometer);
+
+        /*
+        // Print Kilometer object's fields
+        for (var key in window.Kilometer) {
+          if (key) {
+            console.log('Entered test Kilometer.io > before loading > #initialize > it(should stub window.Kilometer with the right methods) : Kilometer.' + key + ' = ' + window.Kilometer[key]);
+          }
+        }
+        */
+
+        // Print Kilometer object's fields
+        /*
+        for (var key in analytics) {
+          // var keys = ['Integrations', 'options', '_user', 'spy', 'stub', 'restore', 'spies', 'called', 'load', 'loaded', 'integration', '']
+
+          if (key) {
+            console.log('Entered test Kilometer.io > before loading > #initialize > it(should stub window.Kilometer with the right methods) : analytics.' + key + ' = ' + analytics[key]);
+          }
+        }
+        */
+
+        /*
+        var integration = analytics.integration('Kilometer.io');
+        console.log('Entered test Kilometer.io > before loading > #initialize > it(should stub window.Kilometer with the right methods) : analytics.integration[Kilometer.io] = ' + integration);
+        for (var m in integration) {
+          if (m) {
+            console.log('Entered test Kilometer.io > before loading > #initialize > it(should stub window.Kilometer with the right methods) : analytics.integration[Kilometer.io].' + m + ' = ' + integration[m]);
+          }
+        }
+        */
+
+        /*
+        var Integrations = analytics.Integrations;
+        for (var _i in Integrations) {
+          if (_i) {
+            console.log('Entered test Kilometer.io > before loading > #initialize > it(should stub window.Kilometer with the right methods) : analytics.Integrations.' + _i + ' = ' + Integrations[_i]);
+          }
+        }
+        */
+
+        methods.forEach(function(method) {
           analytics.assert(window.Kilometer[method]);
         });
       });
 
       it('should set window.Kilometer.app_id', function() {
+        console.log('Entered test Kilometer.io > before loading > #initialize > it(should set window.Kilometer.app_id)');
         analytics.assert(!window.Kilometer);
         analytics.initialize();
         analytics.assert(window.Kilometer.app_id === options.app_id);
       });
 
       it('should call #load', function() {
+        console.log('Entered test Kilometer.io > before loading > #initialize > it(should call #load)');
         analytics.initialize();
         analytics.called(kilometerIntegration.load);
       });
@@ -85,16 +146,29 @@ describe('Kilometer.io', function() {
 
   describe('loading', function() {
     it('should load', function(done) {
+      console.log('Entered test Kilometer.io > loading > it(should load)');
+      // console.log('Entered test Kilometer.io > loading > it(should load) : analytics.load = ' + analytics['load']);
+      // console.log('Entered test Kilometer.io > loading > it(should load) : analytics.load = ' + analytics['load']);
       analytics.load(kilometerIntegration, done);
     });
   });
 
+
   describe('after loading', function() {
     beforeEach(function(done) {
+      sandbox();
       analytics.once('ready', done);
       analytics.initialize();
       analytics.page();
     });
+
+    afterEach(function() {
+      console.log('Entered test Kilometer.io > after loading > afterEach()');
+      if (typeof window.Kilometer !== 'undefined' || window.Kilometer !== null) {
+        window.Kilometer = null;
+      }
+    });
+
 
     describe('#identify', function() {
       beforeEach(function() {
@@ -102,18 +176,19 @@ describe('Kilometer.io', function() {
         analytics.stub(window.Kilometer, 'setUserProperties');
       });
 
-      it('should send traits', function() {
+      it('if no user id is specified - do not do anything', function() {
         analytics.identify({ trait: true, number: 1 });
+        analytics.didNotCall(window.Kilometer.identify);
+        analytics.didNotCall(window.Kilometer.setUserProperties);
+      });
+
+      it('should send traits', function() {
+        analytics.identify('vasya2', { trait: true, number: 1 });
         analytics.called(window.Kilometer.setUserProperties, { trait: true, number: 1 });
       });
 
-      /*
-      it('should alias email to _email', function() {
-        analytics.identify({ trait: true, email: 'email@email.org' });
-        analytics.called(window.Kilometer.setUserProperties, { trait: true, _email: 'email@email.org' });
-      });
-      */
 
+      /*
       it('should send id as handle', function() {
         analytics.identify('id');
         analytics.called(window.Kilometer.identify, 'id');
@@ -155,21 +230,23 @@ describe('Kilometer.io', function() {
         analytics.called(window.Kilometer.identify, 'id');
         analytics.called(window.Kilometer.setUserProperties, { date: '2016-01-01T00:00:00.000Z' });
       });
+      */
     });
 
+    /*
     describe('#track', function() {
       beforeEach(function() {
-        analytics.stub(window.kilometerIntegration, 'track');
+        analytics.stub(window.Kilometer, 'transmitEvent');
       });
 
       it('should send an event', function() {
         analytics.track('event');
-        analytics.called(window.kilometerIntegration.track, 'event');
+        analytics.called(window.Kilometer.transmitEvent, 'event');
       });
 
       it('should send an event and properties', function() {
         analytics.track('event', { property: true });
-        analytics.called(window.kilometerIntegration.track, 'event', { property: true });
+        analytics.called(window.Kilometer.transmitEvent, 'event', { property: true });
       });
 
       it('should flatten nested objects and arrays', function() {
@@ -186,7 +263,7 @@ describe('Kilometer.io', function() {
             ]
           }
         });
-        analytics.called(window.kilometerIntegration.track, 'event', {
+        analytics.called(window.Kilometer.transmitEvent, 'event', {
           property: 3,
           'foo.bar.hello': 'teemo',
           'foo.cheese': '[\"1\",2,\"cheers\"]',
@@ -194,5 +271,6 @@ describe('Kilometer.io', function() {
         });
       });
     });
+    */
   });
 });
